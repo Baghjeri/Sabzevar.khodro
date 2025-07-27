1ec7f76b10aa1cf6115f3297bf7210cd573359ab
Baghjeri <131592100+Baghjeri@users.noreply.github.com>
2025-07-27 04:11:57 +0330

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import { Text, View, StyleSheet } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>سلام! اپلیکیشن سبزوار خودرو آماده شد ✅</Text>
-    </View>
-  );
+{
+  "expo": {
+    "name": "Sabzevar",
+    "slug": "sabzevar",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "assetBundlePatterns": [
+      "**/*"
+    ]
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
